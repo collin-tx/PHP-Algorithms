f1aa7519be69aa016c6aedb28865eaa0b54821c5
Collin Smith <cmsmith.tx@gmail.com>
2020-01-16 16:15:29 -0600

    adding solution

--- array-problems/arrayToHash.php
+++ array-problems/arrayToHash.php
@@ -1,25 +1,41 @@
 <!-- create a function that takes an array and turns it to a hash table -->
 
-<!-- doesn't work, needs revision -->
-
-
 <?php
 function arrayToHash($arr) {
 
-  $hash;
+  // $hash = array_flip($arr);
 
-  for($i=0; $i < count($arr); $i++){
-    if (in_arr($hash, $arr[$i]){
-      $hash[$arr[$i]]++;
-    } else {
-      array_push($hash, $arr[$i]);
-    }
-  }
+  // for($i=0; $i < count($arr); $i++){
+  //   if (in_array($hash, $arr[$i]){
+  //     $hash[$arr[$i]]++;
+  //   } else {
+  //     array_push($hash, $arr[$i]);
+  //   }
+  // }
 
-  return $hash;
+  // return $hash;
+
+
+  // OK, easy way to solve ->
+  return array_count_values($arr);
 }
 
 $a = ['a', 'b', 'a', 'b', 'c', 'c', 'd', 'e'];
-echo arrayToHash($a);
+// // echo arrayToHash($a);
+
+// $newArr = array();
+
+// // foreach($flipped as $item){
+
+// // }
+
+// for($i=0; $i<count($a); $i++){
+//   if(!in_array($a[$i], $newArr)){
+//     array_push($newArr, $a[$i]);
+//   }
+// }
+
+
+// print_r($newArr);
 
 ?>
